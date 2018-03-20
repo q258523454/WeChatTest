7b00a8bddd323566b121010a481648bd7ae536b8
zhangj <258523454@qq.com>
2018-03-20 16:09:49 +0800

    微信公众号消息回复

--- src/main/java/controller/WXServlet.java
+++ src/main/java/controller/WXServlet.java
@@ -1,6 +1,9 @@
 package controller;
 
+import entity.TextMessage;
+import org.apache.log4j.Logger;
 import util.CheckUtil;
+import util.MessageUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,28 +11,75 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 
 public class WXServlet extends HttpServlet {
 
+    private Logger logger = Logger.getLogger(WXServlet.class);
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         String signature = request.getParameter("signature");
-
         String timestamp = request.getParameter("timestamp");
-
         String nonce = request.getParameter("nonce");
-
         String echostr = request.getParameter("echostr");
-
         PrintWriter out = response.getWriter();
-
         if (CheckUtil.checkSignature(signature, timestamp, nonce)) {
-
-        //如果校验成功，将得到的随机字符串原路返回
-
+            //如果校验成功，将得到的随机字符串原路返回
             out.print(echostr);
-
         }
 
     }
+
+    // 超过5秒, 微信服务器不会处理
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long startTime = System.currentTimeMillis();    //获取开始时间
+        response.setCharacterEncoding("UTF-8");         // 避免中文乱码
+        PrintWriter out = response.getWriter();
+        String str = "";
+        try {
+            Map<String, String> map = MessageUtil.xmlToMap(request);
+            // 取元素
+            String ToUserName = map.get("ToUserName");
+            String FromUserName = map.get("FromUserName");
+            String CreateTime = map.get("CreateTime");
+            String MsgType = map.get("MsgType");
+            String Content = map.get("Content");
+            String MsgId = map.get("MsgId");
+            if (MsgType.equals(MessageUtil.REQ_MESSAGE_TYPE_VOICE)) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                TextMessage message = new TextMessage();
+                message.setFromUserName(ToUserName);
+                message.setToUserName(FromUserName);
+                message.setMsgType("text");
+                message.setCreateTime(simpleDateFormat.format(new Date()));
+                message.setContent("Hello World!");
+                str = MessageUtil.textMsgToXml(message);
+            } else if (MsgType.equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) {//判断是否为文本消息类型
+                if (Content.equals("1")) {
+                    str = MessageUtil.initText(ToUserName, FromUserName, "A");
+                } else if (Content.equals("2")) {
+                    str = MessageUtil.initText(ToUserName, FromUserName, "B");
+                } else if (Content.equals("3")) {
+                    str = MessageUtil.initText(ToUserName, FromUserName, MessageUtil.menuText());
+                } else {
+                    str = MessageUtil.initText(ToUserName, FromUserName, "没让你选的就别瞎嘚瑟！！！");
+                }
+            } else if (MsgType.equals(MessageUtil.REQ_MESSAGE_TYPE_EVENT)) {    //判断是否为事件类型
+                String eventType = map.get("Event");
+                if (eventType.equals(MessageUtil.EVENT_TYPE_SUBSCRIBE)) {
+                    str = MessageUtil.initText(ToUserName, FromUserName, MessageUtil.menuText());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis();    //获取结束时间
+        logger.info(("程序运行时间：" + (endTime - startTime) + "ms"));    //输出程序运行时间
+        out.print(str);
+        out.close();
+    }
+
+
 }
--- src/main/java/util/MessageUtil.java
+++ src/main/java/util/MessageUtil.java
@@ -13,6 +13,7 @@ import org.dom4j.io.SAXReader;
 import javax.servlet.http.HttpServletRequest;
 import java.io.InputStream;
 import java.io.Writer;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,123 +21,38 @@ import java.util.Map;
 /**
  * 消息转化工具类
  *
- * @author sun
+ * @author zhangjian
  */
 
 public class MessageUtil {
-
-    /**
-     * 返回消息类型：文本
-     */
-
     public static final String RESP_MESSAGE_TYPE_TEXT = "text";
-
-    /**
-     * 返回消息类型：音乐
-     */
-
     public static final String RESP_MESSAGE_TYPE_MUSIC = "music";
-
-    /**
-     * 返回消息类型：图文
-     */
-
-    public static final String RESP_MESSAGE_TYPE_NEWS = "news";
-
-    /**
-     * 请求消息类型：文本
-     */
-
+    public static final String RESP_MESSAGE_TYPE_NEWS = "news";         // 图文
     public static final String REQ_MESSAGE_TYPE_TEXT = "text";
-
-    /**
-     * 请求消息类型：图片
-     */
-
     public static final String REQ_MESSAGE_TYPE_IMAGE = "image";
-
-    /**
-     * 请求消息类型：链接
-     */
-
-    public static final String REQ_MESSAGE_TYPE_LINK = "link";
-
-    /**
-     * 请求消息类型：地理位置
-     */
-
-    public static final String REQ_MESSAGE_TYPE_LOCATION = "location";
-
-    /**
-     * 请求消息类型：音频
-     */
-
-    public static final String REQ_MESSAGE_TYPE_VOICE = "voice";
-
-    /**
-     * 请求消息类型：推送
-     */
-
-    public static final String REQ_MESSAGE_TYPE_EVENT = "event";
-
-    /**
-     * 事件类型：subscribe(订阅)
-     */
-
-    public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
-
-    /**
-     * 事件类型：unsubscribe(取消订阅)
-     */
-
-    public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
-
-    /**
-     * 事件类型：CLICK(自己定义菜单点击事件)
-     */
-
-    public static final String EVENT_TYPE_CLICK = "CLICK";
+    public static final String REQ_MESSAGE_TYPE_LINK = "link";          // 链接
+    public static final String REQ_MESSAGE_TYPE_LOCATION = "location";  // 地理位置
+    public static final String REQ_MESSAGE_TYPE_VOICE = "voice";        // 音频
+    public static final String REQ_MESSAGE_TYPE_EVENT = "event";        // 推送
+    public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";      // subscribe(订阅)
+    public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";  // unsubscribe(取消订阅)
+    public static final String EVENT_TYPE_CLICK = "CLICK";              // CLICK(自己定义菜单点击事件)
 
     //定义一个私有的静态全局变量来保存该类的唯一实例
-
     private static MessageUtil messageUtil;
 
     /// 构造函数必须是私有的
-
     /// 这样在外部便无法使用 new 来创建该类的实例
-
-    private MessageUtil()
-
-    {
+    private MessageUtil() {
 
     }
 
-    /// 定义一个全局访问点
-
-    /// 设置为静态方法
-
-    /// 则在类的外部便无需实例化就可以调用该方法
-
-    public static MessageUtil getInstance()
-
-    {
-
-        //这里可以保证只实例化一次
-
-        //即在第一次调用时实例化
-
-        //以后调用便不会再实例化
-
-        if (messageUtil == null)
-
-        {
-
+    /// 单例模式
+    public static MessageUtil getInstance() {
+        if (messageUtil == null) {
             messageUtil = new MessageUtil();
-
         }
-
         return messageUtil;
-
     }
 
     /**
@@ -146,55 +62,30 @@ public class MessageUtil {
      * @return
      * @throws Exception
      */
-
-    public Map<String, String> parseXml(HttpServletRequest request) throws Exception {
-
+    public static Map<String, String> xmlToMap(HttpServletRequest request) throws Exception {
         // 将解析结果存储在HashMap中
-
         Map<String, String> map = new HashMap<String, String>();
-
         // 从request中取得输入流
-
         InputStream inputStream = request.getInputStream();
-
         // 读取输入流
-
         SAXReader reader = new SAXReader();
-
         Document document = reader.read(inputStream);
-
         String requestXml = document.asXML();
-
         String subXml = requestXml.split(">")[0] + ">";
-
         requestXml = requestXml.substring(subXml.length());
-
         // 得到xml根元素
-
         Element root = document.getRootElement();
-
         // 得到根元素的全部子节点
-
         List<Element> elementList = root.elements();
-
         // 遍历全部子节点
-
         for (Element e : elementList) {
-
             map.put(e.getName(), e.getText());
-
         }
-
         map.put("requestXml", requestXml);
-
         // 释放资源
-
         inputStream.close();
-
         inputStream = null;
-
         return map;
-
     }
 
     /**
@@ -203,89 +94,26 @@ public class MessageUtil {
      * @param request
      * @return
      */
-
-    public String parseMsgXml(HttpServletRequest request) {
-
+    public static String requestMsgToXml(HttpServletRequest request) {
         String responseMsg = null;
-
         try {
-
             InputStream is = request.getInputStream();
-
             int size = is.available();
-
             byte[] jsonBytes = new byte[size];
-
             is.read(jsonBytes);
-
             responseMsg = new String(jsonBytes, "UTF-8");
-
             is.close();
-
         } catch (Exception e) {
-
             // TODO Auto-generated catch block
-
             e.printStackTrace();
-
         }
-
         return responseMsg;
-
     }
-
-    /**
-     * 文本消息对象转换成xml
-     *
-     * @param textMessage 文本消息对象
-     * @return xml
-     */
-
-    public String textMessageToXml(TextMessage textMessage) {
-
-        xstream.alias("xml", textMessage.getClass());
-
-        return xstream.toXML(textMessage);
-
-    }
-
-    /**
-     * 音乐消息对象转换成xml
-     *
-     * @param musicMessage 音乐消息对象
-     * @return xml
-     */
-
-//    public String musicMessageToXml(MusicMessage musicMessage) {
-//
-//        xstream.alias("xml", musicMessage.getClass());
-//
-//        return xstream.toXML(musicMessage);
-//
-//    }
-
-    /**
-     * 图文消息对象转换成xml
-     *
-     * @param newsMessage 图文消息对象
-     * @return xml
-     */
-
-//    public String newsMessageToXml(NewsMessage newsMessage) {
-//
-//        xstream.alias("xml", newsMessage.getClass());
-//
-//        xstream.alias("item", new Article().getClass());
-//
-//        return xstream.toXML(newsMessage);
-//
-//    }
 
     /**
      * 扩展xstream，使其支持CDATA块
      */
-
-    private XStream xstream = new XStream(new XppDriver()) {
+    public static XStream xstream = new XStream(new XppDriver()) {
         public HierarchicalStreamWriter createWriter(Writer out) {
             return new PrettyPrintWriter(out) {
                 // 对全部xml节点的转换都添加CDATA标记
@@ -297,6 +125,7 @@ public class MessageUtil {
                         cdata = false;
                     }
                 }
+
                 protected void writeText(QuickWriter writer, String text) {
                     if (cdata) {
                         writer.write("<![CDATA[");
@@ -310,5 +139,58 @@ public class MessageUtil {
         }
     };
 
+
+    /**
+     * 文本消息对象转换成xml
+     *
+     * @param textMessage 文本消息对象
+     * @return xml
+     */
+    public static String textMsgToXml(TextMessage textMessage) {
+        xstream.alias("xml", textMessage.getClass());
+        return xstream.toXML(textMessage);
+    }
+
+
+    /**
+     * 音乐消息对象转换成xml
+     * @param musicMessage 音乐消息对象
+     * @return xml
+     */
+//    public String musicMessageToXml(MusicMessage musicMessage) {
+//
+//        xstream.alias("xml", musicMessage.getClass());//
+//        return xstream.toXML(musicMessage);//
+//    }
+
+    /**
+     * 图文消息对象转换成xml
+     * @param newsMessage 图文消息对象
+     * @return xml
+     */
+//    public String newsMessageToXml(NewsMessage newsMessage) {
+//
+//        xstream.alias("xml", newsMessage.getClass());//
+//        xstream.alias("item", new Article().getClass());//
+//        return xstream.toXML(newsMessage);//
+//    }
+
+    public static String menuText(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("欢迎关注公众号，请选择:\n\n");
+        sb.append("1、A。\n");
+        sb.append("2、B。\n\n");
+        sb.append("3、主菜单。\n\n");
+        return sb.toString();
+    }
+    public static String initText(String toUSerName,String fromUserName,String content){
+        TextMessage text = new TextMessage();
+        text.setFromUserName(toUSerName);
+        text.setToUserName(fromUserName);
+        text.setMsgType(MessageUtil.REQ_MESSAGE_TYPE_TEXT);
+        text.setCreateTime(new Date().getTime()+"");
+        text.setContent(content);
+        return MessageUtil.textMsgToXml(text);
+    }
 }
 
